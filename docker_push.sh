be5de5d883374ea16636c89404bdff94bebd4ec0
Leonardo <leonnxrdx_@hotmail.com>
2020-05-18 18:17:44 -0500

    Fix region aws1

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,9 @@
-aws --region $AWS_DEFAULT_REGION ecr get-login-password | docker login --username AWS --password-stdin $AWS_ACCESS_KEY_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+aws --region ${AWS_DEFAULT_REGION} ecr get-login-password \
+    | docker login \
+        --password-stdin \
+        --username AWS \
+        "${AWS_ACCESS_KEY_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"
+
 docker build -t $ECR_REPO_NAME .
 docker tag $ECR_REPO_NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$ECR_REPO_NAME:latest
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$ECR_REPO_NAME:latest

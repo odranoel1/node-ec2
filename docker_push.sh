cece79518a783d7d18e6818317acd1a3e4292020
Leonardo <leonnxrdx_@hotmail.com>
2020-05-18 16:58:22 -0500

    Fix docker push

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,4 @@
+aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCESS_KEY_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 docker build -t $ECR_REPO_NAME .
 docker tag $ECR_REPO_NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$ECR_REPO_NAME:latest
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$ECR_REPO_NAME:latest

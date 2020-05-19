5cf383f77660cf7617e709d41548f9d327afc5ab
Leonardo <leonnxrdx_@hotmail.com>
2020-05-19 11:02:24 -0500

    Add deleteimages ecr, change node image

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 //Para crear rutas del servidor
 router.get('/', (req, res) => {
-  res.send({status: 'Hello world'});
+  res.send({status: 'Hello bitch'});
 });
 
 module.exports = router;
